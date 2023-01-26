1f2740917320e113216a58c2129c4226d357ac21
yicr <yicr@users.noreply.github.com>
2023-01-26 19:18:11 +0900

    deps: add module @yicr/jest-serializer-cdk-asset with jest config update.

--- test/snapshot-plugin.ts
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  test: (val: unknown) => typeof val === 'string',
-  serialize: (val: string) => {
-    return `"${val.replace(/([A-Fa-f0-9]{64}.zip)/, 'HASH.zip')}"`;
-  },
-};
