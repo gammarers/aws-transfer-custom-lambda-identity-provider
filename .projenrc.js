a45c67a88f93009a0d6cc87b7f088faf8f91187c
yicr <yicr@users.noreply.github.com>
2023-01-27 10:30:54 +0900

    chore: add npm access to public, add package description.

--- .projenrc.js
+++ .projenrc.js
@@ -1,22 +1,22 @@
-const { awscdk } = require('projen');
+const { awscdk, javascript } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
   cdkVersion: '2.61.0',
   defaultReleaseBranch: 'main',
   name: '@yicr/transfer-custom-lambda-identity-provider',
+  description: 'This is a Simple Transfer AWS CDK Construct',
   repositoryUrl: 'https://github.com/yicr/transfer-custom-lambda-identity-provider.git',
   keywords: ['aws', 'cdk', 'aws-cdk', 'transfer', 'sftp'],
   deps: [],
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   devDeps: [
     '@yicr/jest-serializer-cdk-asset',
   ],
-  // packageName: undefined,  /* The "name" in package.json. */
   jestOptions: {
     jestConfig: {
       snapshotSerializers: ['<rootDir>/node_modules/@yicr/jest-serializer-cdk-asset'],
     },
   },
+  npmAccess: javascript.NpmAccess.PUBLIC,
 });
 project.synth();
